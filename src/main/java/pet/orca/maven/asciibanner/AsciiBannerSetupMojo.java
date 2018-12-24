4bbdca181524c2be48e7b891d3c459c722b5a93f
Marcos Del Sol Vives <marcos@orca.pet>
2018-12-24 15:41:57 +0100

    Ensure the setup runs only once

--- src/main/java/pet/orca/maven/asciibanner/AsciiBannerSetupMojo.java
+++ src/main/java/pet/orca/maven/asciibanner/AsciiBannerSetupMojo.java
@@ -6,18 +6,22 @@
 package pet.orca.maven.asciibanner;
 
 import org.apache.maven.execution.MavenExecutionRequest;
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.InstantiationStrategy;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.execution.MavenSession;
 
 /**
  * Mojo which sets up the execution listener.
  */
 @Mojo(
-		name="setup"
+		name = "setup",
+		defaultPhase = LifecyclePhase.INITIALIZE,
+		instantiationStrategy = InstantiationStrategy.SINGLETON
 )
 public class AsciiBannerSetupMojo extends AbstractMojo {
 
@@ -34,13 +38,29 @@ public class AsciiBannerSetupMojo extends AbstractMojo {
 	private FileMessage failure;
 
 	/**
+	 * True to ensure the plugin only runs once.
+	 */
+	@Parameter(defaultValue = "true")
+	private boolean runOnce;
+
+	/**
 	 * Maven session.
 	 */
 	@Parameter(defaultValue = "${session}", readonly = true)
 	private MavenSession mavenSession;
 
+	/**
+	 * True if the mojo has been executed at least once.
+	 */
+	private boolean executed = false;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
+		if (runOnce && executed) {
+			return;
+		}
+		executed = true;
+
 		MavenExecutionRequest request = mavenSession.getRequest();
 
 		AsciiBannerExecutionListener abel = new AsciiBannerExecutionListener(getLog());
